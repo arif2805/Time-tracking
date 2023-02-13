4dfa9f240da6c827b64a323888e3efa14f099afe
Md Ariful Islam <arifmohammad2805@gmail.com>
2023-02-13 19:23:44 +0100

    commit

--- images/main.js
+++ images/main.js
@@ -4,22 +4,22 @@ let monthly = document.getElementById("monthly");
 let x=0;
 
 function myFunction(){
-    if (x=0){
-        textWeekCurrnt.innerText="5hrs";
-        textWeekPast.innerText="7hrs";
-        x=1;
-    }
-    else{
-        textWeekCurrnt.innerText="103hrs";
-        textWeekPast.innerText="128hrs";
-        x=2;
-    }
-    console.log(myFunction);
+    // if (x=0){
+    //     textWeekCurrnt.innerText="5hrs";
+    //     textWeekPast.innerText="7hrs";
+    //     x=1;
+    // }
+    // else{
+    //     textWeekCurrnt.innerText="103hrs";
+    //     textWeekPast.innerText="128hrs";
+    //     x=1;
+    // }
+    console.log("Hey it is not workin");
 };
 
 daily.addEventListener("click",myFunction);
-weekly.addEventListener("click",myFunction);
-weekly.addEventListener("click",myFunction);
+// weekly.addEventListener("click",myFunction);
+// monthly.addEventListener("click",myFunction);
 
 
 
